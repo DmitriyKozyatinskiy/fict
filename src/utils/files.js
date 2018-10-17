08fda99aa2525d93b42a1daa58b5d591115fbf84
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2018-10-17 16:35:21 +0300

    Improve excel file loading to prevent popup blocking

--- src/utils/files.js
+++ src/utils/files.js
@@ -2,5 +2,9 @@ import { XLSX_MIME_TYPE } from './../constants';
 
 export const downloadExcelFileFromBase64 = base64Data => {
   const downloadURL = `data:${XLSX_MIME_TYPE};base64,${base64Data}`;
-  window.open(downloadURL, '_blank');
+
+  const downloadLinkElement = document.createElement('a');
+  downloadLinkElement.target = '_blank';
+  downloadLinkElement.href = downloadURL;
+  downloadLinkElement.click();
 };
